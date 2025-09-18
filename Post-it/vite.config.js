6e7436f86900e825a673ff3c3cfabf3fd3abc301
esperanceahouissou <esperanceahouissou@gmail.com>
2025-09-18 09:19:57 +0100

    project

---
+++ Post-it/vite.config.js
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import withMT from '@material-tailwind/html/utils/withMT'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
